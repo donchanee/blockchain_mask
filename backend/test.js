015b4167d46cfa916814d02c962d8b8b57b560df
HanJaehee <gkswogml23@sju.ac.kr>
2020-06-10 16:34:02 +0900

    edit test

--- backend/test.js
+++ backend/test.js
@@ -1,37 +1,6 @@
-const fb = require('./firebase_init');
-require('date-utils');
-const db = fb.firestore();
+let data = new Object();
 
-let userRef = db.collection("users");
+data[8] = 9;
+data[9] = 10;
 
-function test(addr){
-    let arr = new Array();
-
-    return new Promise(resolve => {
-        userRef.get().then(snapshot => {
-            snapshot.forEach(doc => {
-                let result = doc.data();
-                let tmp = {
-                    addr : result['addr'].toLowerCase(),
-                    name : result['name']
-                }
-                arr.push(tmp);
-            }); 
-            resolve(arr);
-        });
-    });
-}
-
-async function test1(){
-    let companyList = new Object();
-    await test("0xc2988556ae24daf3a20b16d3eb4d970e43e3546d").then(result => {
-        companyList = result;
-    });
-    
-    console.log(companyList);
-    for(tmp in companyList){
-        console.log(tmp);
-    }
-}
-
-test1();
+console.log(data['8']);
